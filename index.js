0faf605593cca18cc52490244c290c3afe945c24
augustfull <javadroosta1998@gmail.com>
2022-01-22 16:52:12 +0330

    Update main files and add new features

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+  const panels = document.querySelectorAll(".panel");
+
+  function toggle() {
+    this.classList.toggle("open");
+  }
+
+  function toggleActive() {
+    this.classList.toggle("open-active");
+  }
+
+  panels.forEach((panel) => panel.addEventListener("click", toggle));
+  panels.forEach((panel) =>
+    panel.addEventListener("transitionend", toggleActive)
+  );
+});
